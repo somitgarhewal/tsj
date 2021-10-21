e1fb880207003226c6727fe53465100eaac0afcb
somitgarhewal <somitgarhewal@gmail.com>
2021-10-21 22:48:09 +0530

    add routes and their component structure

---
+++ src/components/aboutus/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+    return (
+        <div>
+            About Us Page Content
+        </div>
+    )
+}
+
+export default AboutUs
---
+++ src/components/carRental/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CarRental = () => {
+    return (
+        <div>
+            Car Rental Page
+        </div>
+    )
+}
+
+export default CarRental
