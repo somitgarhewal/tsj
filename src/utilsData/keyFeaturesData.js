bbc8ef8ea77364cce7f30fec442d4e522b1d5557
somitgarhewal <somitgarhewal@gmail.com>
2021-10-24 03:13:00 +0530

    created key feature component

---
+++ src/utilsData/keyFeaturesData.js
@@ -0,0 +1,23 @@
+export const keyFeaturesData = [
+    {
+        key: 'driver_results',
+        iconClasses: 'fa fa-bar-chart-o',
+        title: 'Drivers Results',
+        desc: `Our Drivers are well trained and played a prominent role by conducting the final inspection ensuring proper vehicle cleanliness, appearance and readiness meet company standards.`,
+        image: require('../assets/bmw-m2.jpeg')
+    },
+    {
+        key: 'culture_and_core_values',
+        iconClasses: 'fa fa-gears',
+        title: 'Our Culture & Core Values',
+        desc: `We are a Group of Passionate Professionals, who always focused on  quality and in-time service to our customers with thier safety. By this we are increasing happy clients day by day.`,
+        image: require('../assets/mercedes-benz-e.jpg')
+    },
+    {
+        key: 'fast_service',
+        iconClasses: 'fa fa-wrench',
+        title: 'Fast Service',
+        desc: `We are available 7 days, 24×7 for our esteemed  customers in just a click or calls. Also provide our Virtual Assistance services to connect anytime with You.`,
+        image: require('../assets/2021_jaguar_xf.jpeg')
+    },
+]
